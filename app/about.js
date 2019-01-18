f3fdf8a7fab18a7d3851e4e4863c15a2251aa7e5
Jan Bölsche <regular.gonzales@googlemail.com>
2019-01-18 23:15:39 +0100

    Styling, auto-scroll boot log, check boot blob availability

--- app/about.js
+++ app/about.js
@@ -1,4 +1,24 @@
 const h = require('hyperscript')
+const setStyles = require('module-styles')('bayofplenty')
+
+setStyles(`
+  ul.versions {
+    display: grid;
+    grid-template-rows: repeat(5, 1fr);
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-auto-flow: row;
+  }
+  body {
+    font-family: sans-serif;
+  }
+  div#log {
+    max-height: 15em;
+    overflow-y: scroll;
+    font-family: monospace;
+  }
+`)
+
+checkBlob()
 
 document.body.appendChild(
   h('div#log', [
@@ -6,6 +26,29 @@ document.body.appendChild(
     h('.container')
   ])
 )
+
+function checkBlob() {
+  const el = h('.boot', 'checking blob ...')
+  document.body.appendChild(el)
+  fetch('/boot', {
+    method: 'HEAD'
+  }).then(response => {
+    if (!response.ok) {
+      log('error', 'Server response for /boot', response.statusText)
+      el.innerText = response.statusText
+      return
+    }
+    const etag = response.headers.get('etag')
+    log('important', `Blob available: ${etag}`)
+    el.innerText = etag
+    setTimeout( ()=>{
+      location.href = '/boot'
+    })
+  }).catch(err => {
+    log('error', 'Failure requesting HEAD /boot', err.message)
+    el.innerText = 'FAIL'
+  })
+}
 
 function log(type, ...args) {
   console.log(type, args.join(' '))
@@ -19,6 +62,8 @@ function log(type, ...args) {
       [type, ...args.map(x => h('span', x))]
     )
   )
+  const p = el.parentElement
+  p.scrollTop = p.scrollHeight
 }
 
 // API called from Electron process
