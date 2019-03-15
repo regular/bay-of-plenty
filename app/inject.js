ace5af3e88b11e97f9cfa23e880e690a0fdf5b7e
Jan Bölsche <regular.gonzales@googlemail.com>
2019-03-15 02:30:27 +0100

    bump version

--- app/inject.js
+++ app/inject.js
@@ -36,7 +36,7 @@ module.exports = function inject(electron, fs, log, sbot) {
       log('window closed')
       win = null
     })
-    //win.openDevTools()
+    win.openDevTools()
 
     server({}, (err, ssb, config, myid, browserKeys) => {
       if (err) {
