4b2ea48e6309933132e61c42e053f25a1e2f2392
Jan Bölsche <regular.gonzales@googlemail.com>
2020-05-10 12:02:39 +0200

    Store known avatars, boot menu improvments

--- app/avatar-update.js
+++ app/avatar-update.js
@@ -1,9 +1,40 @@
+const fs = require('fs')
+const {join} = require('path')
+
 const Notify = require('pull-notify')
 const pull = require('pull-stream')
 const cat = require('pull-cat')
 const debug = require('debug')('bop:avatar-update')
+const networkDir = require('./lib/get-networks-dir')()
 
+const storagePath = join(networkDir, '..', 'avatars.json')
+const cold = loadColdStorage()
 const entries = {}
+
+function loadColdStorage() {
+  debug(`Reading ${storagePath}`)
+  try {
+    return JSON.parse(fs.readFileSync(storagePath))
+  } catch(err) {
+    debug(`Unable to read avatars: ${err.message}`)
+    return {}
+  }
+}
+
+function saveColdStorage(cold) {
+  debug(`Writing ${storagePath}`)
+  try {
+    fs.writeFileSync(storagePath, JSON.stringify(cold), 'utf8')
+  } catch(err) {
+    debug(`Unable to write avatars: ${err.message}`)
+  }
+}
+
+function setCold(network, id, avatar) {
+  if (!cold[network]) cold[network] = {}
+  cold[network][id] = avatar
+  saveColdStorage(cold)
+}
 
 function getEntry(network, id) {
   const key = `${network} ${id}`
@@ -11,7 +42,7 @@ function getEntry(network, id) {
   if (!entry) {
     entry = {
       notify: Notify(),
-      avatar: {
+      avatar: (cold[network] && cold[network][id]) || {
         name: null,
         image: null
       }
@@ -25,6 +56,7 @@ function avatarUpdate(network, id, key, value) {
   debug(`Updating avatar ${key} to ${value}, ${network} ${id}`)
   const entry = getEntry(network, id)
   entry.avatar[key] = value
+  setCold(network, id, entry.avatar)
   entry.notify(entry.avatar)
 }
 
