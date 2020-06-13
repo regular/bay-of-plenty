420596048cb298f53b534cd5fd6d13b2632caaf7
Jan Bölsche <regular.gonzales@googlemail.com>
2020-06-13 20:38:03 +0200

    version bumps

--- app/index.js
+++ app/index.js
@@ -1,3 +1,8 @@
+/* uncomment to crash when there's no native crypto
+console.log('sodium-native ...')
+const sn = require('sodium-native')
+console.log(sn.version)
+*/
 const electron = require('electron')
 const fs = require('fs')
 const sbot = require('./sbot')
