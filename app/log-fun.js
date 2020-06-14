78bb7c2752863d05476af731aa1bb0ea353ef2a5
Jan Bölsche <regular.gonzales@googlemail.com>
2020-06-14 22:27:06 +0200

    Add console-with-style

--- app/log-fun.js
+++ app/log-fun.js
@@ -1,4 +1,8 @@
 const debug = require('debug')('browser-console')
+const Format = require('console-with-style')
+const supportsColor = require('supports-color')
+const level = (supportsColor.stdout && supportsColor.stdout.level) || 0
+const format = Format(level)
 
 module.exports = function() {
   let currUrl
@@ -16,7 +20,8 @@ module.exports = function() {
     if (!values.length) {
       values.unshift(consoleMessage.text())
     }
-    const text = values.map(stringify).join(' ')
+    const text = typeof values[0] == 'string' ? format.apply(null, values)
+      : values.map(stringify).join(' ')
     debug(`${loc} ${type} ${text}`)
   }
 }
