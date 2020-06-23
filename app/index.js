0fcb862701d204c287e8b2c958527b6283e1fcbd
Jan Bölsche <regular.gonzales@googlemail.com>
2020-06-23 13:41:09 +0200

    Add env vars: THROW_SODIUM_NATIVE and THROW_DEPRECATION

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
-/* uncomment to crash when there's no native crypto
-console.log('sodium-native ...')
-const sn = require('sodium-native')
-console.log(sn.version)
-*/
+if (process.env.THROW_SODIUM_NATIVE) {
+  console.log('sodium-native ...')
+  const sn = require('sodium-native')
+  console.log(sn.version)
+}
+if (process.env.THROW_DEPRECATION) {
+  process.throwDeprecation=true
+}
+
 const electron = require('electron')
 const sbot = require('./sbot')
 
-//process.throwDeprecation=true
 require('./inject.js')(electron, sbot)
