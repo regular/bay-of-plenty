5805ece630a27b8f4ae0eef21287b6daf285a900
Jan Bölsche <regular.gonzales@googlemail.com>
2020-10-15 10:23:29 +0200

    Fix endless recurson if .trerc is not found (launch local)

--- app/lib/locate-trerc.js
+++ app/lib/locate-trerc.js
@@ -4,6 +4,7 @@ const {parse, join} = require('path')
 module.exports = function locate(path) {
   if (fs.existsSync(join(path, '.trerc'))) return path
   const {dir} = parse(path)
-  if (!dir) throw new Error('Unable to locate .trerc')
+  if (!dir || dir == path) throw new Error('Unable to locate .trerc')
+  console.log(dir)
   return locate(dir)
 }
