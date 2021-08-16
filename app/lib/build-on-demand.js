d9173ee10c2e4191f587902cf77d1f0e29b74c40
Jan Bölsche <regular.gonzales@googlemail.com>
2021-08-16 08:51:13 +0200

    Add meta data to build-on-demand apps

--- app/lib/build-on-demand.js
+++ app/lib/build-on-demand.js
@@ -1,8 +1,10 @@
+const fs = require('fs')
 const {parse} = require('url')
-const {basename} = require('path')
+const {dirname, basename} = require('path')
 const crypto = require('crypto')
 const debug = require('debug')('bop:build-on-demand')
 const compile = require('tre-compile/compile')
+const pkgUp = require('pkg-up')
 
 const hyperstream = require('hyperstream')
 const BufferList = require('bl')
@@ -11,6 +13,7 @@ module.exports = async function(ssb, page, filename, opts) {
   opts = opts || {}
   const random = basename(filename) + crypto.randomBytes(32).toString('hex')
   const origin = opts.origin || 'http://localhost/'
+  const onMeta = opts.onMeta || (()=>{})
   const base = opts.base || `${origin}blobs/get/`
 
   ssb.ws.use(function(req, res, next) {
@@ -18,7 +21,9 @@ module.exports = async function(ssb, page, filename, opts) {
     if(req.method !== 'GET' || u.pathname !== '/' + random) return next()
     debug('request for %s', filename)
 
-    compile(filename, (err, result) => {
+    const metaPromise = getMeta(filename)
+
+    compile(filename, async (err, result) => {
       if (err) { 
         debug('compile failed')
         console.error(`error compiling: ${err.message} ${err.annotated}`)
@@ -27,6 +32,15 @@ module.exports = async function(ssb, page, filename, opts) {
       }
 
       debug('compile done')
+      let meta
+      try{
+        meta = await metaPromise
+      } catch(e) {
+        console.error(e.message)
+        meta = {}
+      }
+      meta.sha = result.sha
+      onMeta(meta)
       debug('sending response')
       res.setHeader(
         'Content-Security-Policy', `script-src 'sha256-${result.sha}';`
@@ -46,3 +60,15 @@ module.exports = async function(ssb, page, filename, opts) {
   await page.goto(`${origin}${random}`)
 }
 
+async function getMeta(filename) {
+  return new Promise( async (resolve, reject) =>{
+    const pkgpath = await pkgUp({cwd: dirname(filename)})
+    if (!pkgpath) {
+      throw new Error('No package.json found.')
+    }
+    fs.readFile(pkgpath, (err, buf)=>{
+      if (err) return reject(err)
+      resolve(JSON.parse(buf))
+    })
+  })
+}
