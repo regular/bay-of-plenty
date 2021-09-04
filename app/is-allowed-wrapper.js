2217ec3d29e6e093fdbbdc5e069814111fe8014c
Jan Bölsche <regular.gonzales@googlemail.com>
2021-09-04 12:16:36 +0200

    Fix permission API wrapper fro types other than async

--- app/is-allowed-wrapper.js
+++ app/is-allowed-wrapper.js
@@ -1,47 +1,54 @@
 const pull = require('pull-stream')
 const defer = require('pull-defer')
+const debug = require('debug')('bop:appperms-wrap')
 
 module.exports = function(isAllowed) {
   return function wrapper(fn, type, path) {
     if (type == 'sync') type = 'async'
     let f = function() {
       const args = Array.from(arguments)
-      const ia_args = [path, args, err=>{
+      
+      const onReply = err=>{
         if (!err) {
           return fn.apply(this, args)
         } else {
           const cb = args.slice(-1)[0]
           cb(err)
         }
-      }]
-      isAllowed.apply(this, ia_args)
-
+      }
+      isAllowed.call(this, path, args, onReply)
     }
+
     if (type == 'source') {
       f = function() {
         const args = Array.from(arguments)
         const deferred = defer.source()
-        isAllowed(path, args, err=>{
+        const onReply = err=>{
           if (!err) {
             deferred.resolve(fn.apply(this, args))
           } else {
             deferred.resolve(pull.error(err))
           }
-        })
+        }
+
+        isAllowed.call(this, path, args, onReply)
         return deferred
       }
     }
+
     if (type == 'sink') {
       f = function() {
         const args = Array.from(arguments)
         const deferred = defer.sink()
-        isAllowed(path, args, err=>{
+        const onReply = err=>{
           if (!err) {
             deferred.resolve(fn.apply(this, Array.from(arguments)))
           } else {
             deferred.resolve(function (read) {read(err, ()=>{})})
           }
-        })
+        }
+
+        isAllowed.call(this, path, args, onReply)
         return deferred
       }
     }
